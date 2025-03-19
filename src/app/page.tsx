66755e5936e1c66b882bc8aad84fc08cbc08edd4
Hor Kah Yee <kahyee0423@gmail.com>
2025-03-19 13:35:39 +0800

    done splash-screen amendments

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import SplashScreen from "./SplashScreen/page";
 import HeroSection from "./Hero/page";
 import Navbar from "./NavBar/page";
@@ -10,14 +10,26 @@ const Home = () => {
   const [showSplash, setShowSplash] = useState(true);
   const [isDrawerOpen, setIsDrawerOpen] = useState(false);
 
+  useEffect(() => {
+    const hasSeenSplash = localStorage.getItem("hasSeenSplash");
+    if (hasSeenSplash === "true") {
+      setShowSplash(false); 
+    }
+  }, []);
+
+  const handleSplashComplete = () => {
+    localStorage.setItem("hasSeenSplash", "true"); 
+    setShowSplash(false);
+  };
+
   return (
     <>
       {showSplash ? (
-        <SplashScreen onComplete={() => setShowSplash(false)} />
+        <SplashScreen onComplete={handleSplashComplete} />
       ) : (
         <div className="min-h-screen flex flex-col w-full">
           <Navbar />
-          <main className="flex-grow min-h-[calc(100vh-100px)]"> 
+          <main className="flex-grow min-h-[calc(100vh-100px)]">
             <HeroSection isDrawerOpen={isDrawerOpen} setIsDrawerOpen={setIsDrawerOpen} />
             <ContactDrawer isOpen={isDrawerOpen} setIsOpen={setIsDrawerOpen} />
           </main>
